b417acc9a68452f65697f8aa6e2ac9b7c6a4bf8f
许可 <xuke1668@163.com>
2019-03-29 10:38:54 +0800

    修改获取时间的方法

--- src/tool.py
+++ src/tool.py
@@ -134,15 +134,15 @@ def get_error_info():
     return error_field[0] if error_field else ("", "", "", "")
 
 
-def get_datetime(now=None, years=0, month=0, week=0, day=0, hour=0, minute=0, second=0):
+def get_datetime(now=None, year=0, month=0, week=0, day=0, hour=0, minute=0, second=0):
     if now is None:
         now = datetime.now()
-    res = now + relativedelta(years=years, months=month, weeks=week, days=day, hours=hour, minutes=minute, seconds=second)
+    res = now + relativedelta(years=int(year), months=int(month), weeks=int(week), days=int(day), hours=int(hour), minutes=int(minute), seconds=int(second))
     return res
 
 
-def get_timestamp(now=None, month=0, week=0, day=0, hour=0, minute=0, second=0):
-    dt = get_datetime(now, month, week, day, hour, minute, second)
+def get_timestamp(now=None, year=0, month=0, week=0, day=0, hour=0, minute=0, second=0):
+    dt = get_datetime(now, year, month, week, day, hour, minute, second)
     return int(time.mktime(dt.timetuple()))
 
 
